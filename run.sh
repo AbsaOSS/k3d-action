a3f96f9978523acfb8f853bb7a4071400639ace1
kuritka <kuritka@gmail.com>
2021-04-15 17:28:08 +0200

    k3d-action user specifies k3s version explicitly Related to #19
    There is an increased demand for k3d-action to have a configurable k3s version.
    Following implementation keeps responsibility on k3s version on users.
    If user doesn't specify k3s version via configuration or action argument, the k3d will choose version.
    
    PR affected:
     - implementation
     - tests
     - README.md
    
    I'm expecting to release k3d-action@v1.4.0, that's why I replaced examples in README.md to use AbsaOSS/k3d-action@v1.4.0 instead of AbsaOSS/k3d-action@v1.3.1

--- run.sh
+++ run.sh
@@ -25,8 +25,7 @@ CYAN=
 RED=
 NC=
 K3D_URL=https://raw.githubusercontent.com/rancher/k3d/main/install.sh
-K3D_VERSION=v4.2.0
-K3S_VERSION=docker.io/rancher/k3s:v1.20.4-k3s1
+K3D_VERSION=v4.4.1
 DEFAULT_NETWORK=k3d-action-bridge-network
 DEFAULT_SUBNET=172.16.0.0/24
 NOT_FOUND=k3d-not-found-network
@@ -131,7 +130,7 @@ deploy(){
     curl --silent --fail ${K3D_URL} | TAG=${K3D_VERSION} bash
 
     echo -e "\existing_network${YELLOW}Deploy cluster ${CYAN}$name ${NC}"
-    eval "k3d cluster create $name --wait $arguments --image ${K3S_VERSION} --network $network $registryArg"
+    eval "k3d cluster create $name --wait $arguments --network $network $registryArg"
     wait_for_nodes
 }
 
